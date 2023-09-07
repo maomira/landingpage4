72939b6740946b94408ee51de540aa4f4febad4c
maomira <141223548+maomira@users.noreply.github.com>
2023-09-07 20:02:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,44 @@
 // script.js
 
 const sections = document.querySelectorAll(".section");
-let scrolling = false;
+let currentSection = 0;
+let isScrolling = false;
 
 function scrollToSection(index) {
-    if (!scrolling) {
-        scrolling = true;
+    if (!isScrolling) {
+        isScrolling = true;
         sections[index].scrollIntoView({ behavior: "smooth" });
         setTimeout(() => {
-            scrolling = false;
+            isScrolling = false;
         }, 1000); // Adjust the delay as needed to match the scroll duration
     }
 }
 
 document.addEventListener("keydown", (e) => {
-    if (e.key === "ArrowDown") {
+    if (e.key === "ArrowDown" || e.key === "ArrowUp") {
         e.preventDefault();
-        const currentIndex = Array.from(sections).findIndex((section) => section.getBoundingClientRect().top >= 0);
-        const nextIndex = (currentIndex + 1) % sections.length;
-        scrollToSection(nextIndex);
+        if (!isScrolling) {
+            if (e.key === "ArrowDown") {
+                currentSection = (currentSection + 1) % sections.length;
+            } else if (e.key === "ArrowUp") {
+                currentSection = (currentSection - 1 + sections.length) % sections.length;
+            }
+            scrollToSection(currentSection);
+        }
     }
 });
 
 document.addEventListener("wheel", (e) => {
-    if (e.deltaY > 0) {
-        const currentIndex = Array.from(sections).findIndex((section) => section.getBoundingClientRect().top >= 0);
-        const nextIndex = (currentIndex + 1) % sections.length;
-        scrollToSection(nextIndex);
+    if (!isScrolling) {
+        isScrolling = true;
+        if (e.deltaY > 0) {
+            currentSection = (currentSection + 1) % sections.length;
+        } else {
+            currentSection = (currentSection - 1 + sections.length) % sections.length;
+        }
+        scrollToSection(currentSection);
+        setTimeout(() => {
+            isScrolling = false;
+        }, 1000); // Adjust the delay as needed to match the scroll duration
     }
 });
