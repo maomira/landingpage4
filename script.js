10a9bfc7630944dad881a6701506f581d4651d24
maomira <141223548+maomira@users.noreply.github.com>
2023-09-08 01:29:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,49 @@
 const sections = document.querySelectorAll(".section");
 let currentSection = 0;
-let isScrolling = false;
 
 // Disable manual scrolling by setting overflow-y to hidden
 document.body.style.overflowY = "hidden";
 
-function scrollToSection(direction) {
-    if (!isScrolling) {
-        isScrolling = true;
-        if (direction === "next") {
-            currentSection = (currentSection + 1) % sections.length;
-        } else if (direction === "prev") {
-            currentSection = (currentSection - 1 + sections.length) % sections.length;
-        }
-        sections[currentSection].scrollIntoView({ behavior: "smooth" });
-    }
+// Function to scroll to a specific section
+function scrollToSection(index) {
+    const section = sections[index];
+    const offsetTop = section.offsetTop;
+    window.scrollTo({
+        top: offsetTop,
+        behavior: "smooth"
+    });
 }
 
 // Function to scroll to the next section automatically
 function scrollToNextSection() {
     currentSection = (currentSection + 1) % sections.length;
-    sections[currentSection].scrollIntoView({ behavior: "smooth" });
+    scrollToSection(currentSection);
+}
+
+// Function to scroll to the previous section automatically
+function scrollToPrevSection() {
+    currentSection = (currentSection - 1 + sections.length) % sections.length;
+    scrollToSection(currentSection);
 }
 
 // Set an interval to trigger automatic scrolling (adjust the interval as needed)
 setInterval(scrollToNextSection, 5000); // Scroll to next section every 5 seconds
 
 document.addEventListener("keydown", (e) => {
-    if ((e.key === "ArrowDown" || e.key === "ArrowUp") && !isScrolling) {
-        e.preventDefault();
+    if (!e.repeat) {
         if (e.key === "ArrowDown") {
-            scrollToSection("next"); // Scroll to the next section
+            scrollToNextSection(); // Scroll to the next section
         } else if (e.key === "ArrowUp") {
-            scrollToSection("prev"); // Scroll to the previous section
+            scrollToPrevSection(); // Scroll to the previous section
         }
     }
 });
 
 document.addEventListener("wheel", (e) => {
-    if (!isScrolling) {
-        isScrolling = true;
-        e.preventDefault(); // Prevent default mouse wheel behavior
-        if (e.deltaY > 0) {
-            scrollToSection("next"); // Scroll to the next section
-        } else {
-            scrollToSection("prev"); // Scroll to the previous section
-        }
+    e.preventDefault(); // Prevent default mouse wheel behavior
+    if (e.deltaY > 0) {
+        scrollToNextSection(); // Scroll to the next section
+    } else {
+        scrollToPrevSection(); // Scroll to the previous section
     }
 });
